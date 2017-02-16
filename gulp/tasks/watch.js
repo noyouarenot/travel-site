faa4b52240c33193a77338fa58518ca696e7d635
Chenghua <uguys@qq.com>
2017-02-16 08:32:07 -0800

    completed desktop and mobile header styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -20,6 +20,10 @@ gulp.task('watch',function(){
 		gulp.start('cssInject');
 		});
 
+	watch('./app/assets/scripts/**/*.js',function(){
+		gulp.start('scriptsRefresh');
+	});
+
 });
 
 
@@ -28,4 +32,8 @@ gulp.task('cssInject',['styles'], function(){
 
 	return gulp.src('./app/temp/styles/styles.css')
 	.pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh',['scripts'], function(){
+	browserSync.reload();
 });
