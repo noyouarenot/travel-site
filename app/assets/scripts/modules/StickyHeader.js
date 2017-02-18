976672beff16098a771798b87dec401e5f9c9074
Chenghua <uguys@qq.com>
2017-02-18 10:58:39 -0800

    sticky header modified

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -4,7 +4,7 @@ import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
 	constructor(){
-		this.lazyImages =$(".lazyload");
+		this.lazyImages = $(".lazyload");
 		this.siteHeader = $(".site-header");
 		this.headerTriggerElement = $(".large-hero__title");
 		this.createHeaderWaypoint();
